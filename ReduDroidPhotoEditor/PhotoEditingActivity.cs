70e29d6d212e3946d374146bf2dfb0e89aeb19da
hjcf@cin.ufpe.br <hjcf@cin.ufpe.br@014f759d-073b-cb10-25ad-e8457d75cdb0>
2013-01-13 18:23:43 +0000

    Check-in inicial do módulo ReduDroidPhotoEditor

---
+++ ReduDroidPhotoEditor/PhotoEditingActivity.cs
@@ -0,0 +1,85 @@
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Point = Android.Graphics.Point;
+
+namespace ReduDroidPhotoEditor
+{
+	[Activity(ScreenOrientation = ScreenOrientation.Landscape, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden)]
+	[IntentFilter(new[] {"EDIT_PHOTO"})]
+	public sealed class PhotoEditingActivity : AndroidGameActivity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			PhotoEditing.Activity = this;
+			PhotoEditing photoEditing = new PhotoEditing ();
+
+			SetContentView (photoEditing.Window);
+
+			photoEditing.Run ();
+		}
+
+		public class PhotoEditing : Game
+		{
+			private GraphicsDeviceManager graphics;
+			private SpriteBatch spriteBatch;
+			private Texture2D image;
+			private Texture2D alphaChannel;
+			private Point screenResolution;
+			private Vector2 displayScale;
+			private Vector2 origin;
+			private Vector2 center;
+			private float rotation;
+
+			public PhotoEditing ()
+			{
+				screenResolution = Utils.GetScreenResolution (Activity);
+
+				graphics = new GraphicsDeviceManager (this);
+				graphics.IsFullScreen = true;
+				graphics.PreferredBackBufferWidth = screenResolution.X;
+				graphics.PreferredBackBufferHeight = screenResolution.Y;
+				graphics.SupportedOrientations = DisplayOrientation.LandscapeLeft;
+
+				Content.RootDirectory = "Content";
+			}
+
+			protected override void LoadContent ()
+			{
+				spriteBatch = new SpriteBatch (GraphicsDevice);
+
+				image = Texture2D.FromStream (GraphicsDevice, Utils.ByteArray2Stream (Activity.Intent.GetByteArrayExtra ("imageBytes")));
+				alphaChannel = Content.Load<Texture2D> ("Images/alphaChannel");
+
+				displayScale = image.Width <= screenResolution.X && image.Height <= screenResolution.Y ? Vector2.One : new Vector2 ((float)screenResolution.X / image.Width, (float)screenResolution.Y / image.Height);
+				origin = new Vector2 (image.Width / 2.0F, image.Height / 2.0F);
+				center = new Vector2 ((screenResolution.X - (image.Width * displayScale.X)) / 2, (screenResolution.Y - (image.Height * displayScale.Y)) / 2) + (origin * displayScale);
+			}
+
+			protected override void Draw (GameTime gameTime)
+			{
+				GraphicsDevice.Clear (Color.CornflowerBlue);
+
+				spriteBatch.Begin (SpriteSortMode.Immediate, BlendState.AlphaBlend);
+
+				SamplerState samplerState = GraphicsDevice.SamplerStates [0];
+				samplerState.AddressU = TextureAddressMode.Wrap;
+				samplerState.AddressV = TextureAddressMode.Wrap;
+
+				spriteBatch.Draw (alphaChannel, Vector2.Zero, new Rectangle (0, 0, screenResolution.X, screenResolution.Y), Color.White);
+
+				samplerState.AddressU = TextureAddressMode.Clamp;
+				samplerState.AddressV = TextureAddressMode.Clamp;
+
+				spriteBatch.Draw (image, center, null, Color.White, MathHelper.ToRadians (rotation), origin, displayScale, SpriteEffects.None, 0);
+				spriteBatch.End ();
+
+				base.Draw (gameTime);
+			}
+		}
+	}
+}
